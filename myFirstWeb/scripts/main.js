a93651587f5f10f0bc137afd92d1f66cc8f61afc
EttyGin <ettyg325@gmail.com>
2023-11-16 01:53:07 +0200

    first commit

---
+++ myFirstWeb/scripts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let massage1 = "Remember, I am working on it!";
+let massage2 = " Will yo come back?";
+
+//alert(massage1);//הדפסה
+
+//let res1 = confirm(massage2);
+//alert(`You said ${res1}`);
